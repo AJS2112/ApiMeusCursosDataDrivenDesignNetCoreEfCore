7074ed8fa0b81673d8966ea23d2261ac1b85f95e
Antonio Sucre <ajs2104@gmail.com>
2020-12-20 16:24:42 -0300

    Add Context to CategoryControllers

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using MeusCursos.Data;
 using MeusCursos.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeusCursos.Controllers
 {
@@ -10,46 +13,100 @@ namespace MeusCursos.Controllers
     {
         [HttpGet]
         [Route("")]
-        public async Task<ActionResult<List<Category>>> Get()
+        public async Task<ActionResult<List<Category>>> Get(
+            [FromServices] DataContext context
+        )
         {
-            return new List<Category>();
+            var categories = await context.Categories.AsNoTracking().ToListAsync();
+            return Ok(categories);
         }
 
         [HttpGet]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> GetById(int id)
+        public async Task<ActionResult<Category>> GetById(
+            int id,
+            [FromServices] DataContext context
+            )
         {
-            return new Category();
+            var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(category);
         }
 
         [HttpPost]
         [Route("")]
-        public async Task<ActionResult<List<Category>>> Post([FromBody] Category model)
+        public async Task<ActionResult<List<Category>>> Post(
+            [FromBody] Category model,
+            [FromServices] DataContext context
+        )
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            return Ok(model);
+            try
+            {
+                context.Add(model);
+                await context.SaveChangesAsync();
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Não foi possivel criar a categoria, erro {ex.Message}" });
+            }
         }
 
         [HttpPut]
         [Route("{id:int}")]
-        public async Task<ActionResult<List<Category>>> Put(int id, [FromBody] Category model)
+        public async Task<ActionResult<List<Category>>> Put(
+            int id,
+            [FromBody] Category model,
+            [FromServices] DataContext context
+            )
         {
             if (model.Id != id)
                 return NotFound(new { message = "Categoria no encontrada" });
 
             if (ModelState.IsValid)
                 return BadRequest(ModelState);
+            try
+            {
+                context.Entry<Category>(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest(new { message = $"Este registro já foi atualizado" });
 
-            return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Não foi possivel atualizar a categoria, erro: {ex.Message}" });
+            }
         }
 
         [HttpDelete]
         [Route("{id:int}")]
-        public async Task<ActionResult<List<Category>>> Delete(int id)
+        public async Task<ActionResult<List<Category>>> Delete(
+            int id,
+            [FromServices] DataContext context
+            )
         {
-            return Ok();
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+                return NotFound(new { message = "Categoria não encontrada" });
+
+            try
+            {
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+                return Ok(new { message = "Categoria removida com sucesso" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Não foi possível remover sua categoria, erro: {ex.Message}" });
+            }
         }
     }
 }
